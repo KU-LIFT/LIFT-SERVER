aad061ada07c21c311848fa7b388b9d1b486fd2c
최창규 <chang4293@gmail.com>
2025-06-23 17:06:33 +0900

    task response에 projectKey 추가

--- src/main/java/com/kulift/lift/task/dto/TaskResponse.java
+++ src/main/java/com/kulift/lift/task/dto/TaskResponse.java
@@ -15,6 +15,7 @@ public class TaskResponse {
 	private Long id;
 	private String name;
 	private String description;
+	private String projectKey;
 	private Long columnId;
 	private String assignee;
 	private Priority priority;
@@ -32,6 +33,7 @@ public class TaskResponse {
 			.id(task.getId())
 			.name(task.getName())
 			.description(task.getDescription())
+			.projectKey(task.getColumn().getBoard().getProjectKey())
 			.columnId(task.getColumn().getId())
 			.assignee(task.getAssignee() != null ? task.getAssignee().getName() : null)
 			.priority(task.getPriority())
